5f9cc35d493ca5358ba92e330857a6866955bad9
Akhil <akhilgopakumar2892003@gmail.com>
2025-01-15 00:19:27 +0530

    Added dummy ui for homescreen

--- lib/screens/dashboard/homeScreen/home_screen.dart
+++ lib/screens/dashboard/homeScreen/home_screen.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import '../../../widgets/search_bar.dart' as custom;
 
+import 'package:flutter/material.dart';
+
 class HomeScreen extends StatelessWidget {
+  final List<String> devices = [
+    'Device 1',
+    'Device 2',
+    'Device 3',
+    'Device 4',
+    'Device 5',
+    'Device 6',
+  ]; // Sample data
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,13 +21,32 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          custom.SearchBar(), // Correct usage of the widget
+          SizedBox(height: 10),
+          custom.SearchBar(),// Spacer
           Expanded(
-            child: Center(
-              child: Text(
-                'Data will be displayed here',
-                style: TextStyle(fontSize: 16),
+            child: GridView.builder(
+              padding: EdgeInsets.all(10),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // Adjust to 3 for three columns
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 3 / 2,
               ),
+              itemCount: devices.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 4,
+                  child: Center(
+                    child: Text(
+                      devices[index],
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ],
